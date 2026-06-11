60e6b4aa087c3f1c4c942795051741afd6f8da6d
shirukalan9 <127198882+shirukalan9@users.noreply.github.com>
2026-06-11 22:00:12 +0000

    native: Hotfix invalid OutputLayersEnumerator size access
    - Revert clientCompositionLayers and outLayerFEs reserve() calls in Output.cpp,
      as the getOutputLayersOrderedByZ() method returns an Enumerator without a size() method.

--- services/surfaceflinger/CompositionEngine/src/Output.cpp
+++ services/surfaceflinger/CompositionEngine/src/Output.cpp
@@ -1545,8 +1545,6 @@ renderengine::DisplaySettings Output::generateClientCompositionDisplaySettings(
 std::vector<LayerFE::LayerSettings> Output::generateClientCompositionRequests(
       bool supportsProtectedContent, ui::Dataspace outputDataspace, std::vector<LayerFE*>& outLayerFEs) {
     std::vector<LayerFE::LayerSettings> clientCompositionLayers;
-    clientCompositionLayers.reserve(getOutputLayersOrderedByZ().size());
-    outLayerFEs.reserve(getOutputLayersOrderedByZ().size());
     ALOGV("Rendering client layers");
 
     const auto& outputState = getState();
